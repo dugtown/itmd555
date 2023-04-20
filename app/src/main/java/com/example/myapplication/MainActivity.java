363847d574098fa5c6fc20d5ae5549a5c15e7d10
dugtown <131331495+dugtown@users.noreply.github.com>
2023-04-20 00:08:12 -0500

    Test printline pull

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -85,6 +85,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
             String keyIdentifier = String.valueOf(position);
             i.putExtra("carValue", keyIdentifier);
             startActivity(i);
+            System.out.println("TEST");
         }
 
     }
